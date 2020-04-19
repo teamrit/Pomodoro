dcea248d471c52d7f359e0a842a5e2552b0c6baf
Amrit <amritpalsingha4@gmail.com>
2020-04-19 02:10:23 -0400

    🎉 init commit - Pomodoro

---
+++ Greetings.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using Pomodoro.Src;
+
+namespace Pomodoro
+{
+    /// <summary>
+    /// Interaction logic for Greetings.xaml
+    /// </summary>
+    public partial class Greetings : Window
+    {
+        public Greetings()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButtonClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("button clicked");
+            var pomodoro = Pomodoro.Src.Pomodoro._pomodoro;
+            var interval = new TimeSpan(0, 0, 1);
+            var nextTick = pomodoro + interval;
+            while (true)
+            {
+                while (pomodoro < nextTick)
+                {
+                    Thread.Sleep(nextTick - pomodoro);
+                }
+
+                nextTick += interval;
+                // Notice we're adding onto when the last tick was supposed to be, not when it is now
+                // Insert tick() code here
+                Console.Write("{0} interval:{1}\n", nextTick, interval);
+
+            }
+        }
+    }
+}
---
+++ Src/Pomodoro.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Pomodoro.Src
+{
+    public class Pomodoro
+    {
+        public static DateTime _pomodoro = new DateTime(2020,01,01,00,25,00);
+    };
+}
